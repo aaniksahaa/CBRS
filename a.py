dd75437309523b68fea3b254727dd58c6be4577e
Anik Saha <aaniksahaa.2001@gmail.com>
2025-04-11 18:20:34 +0600

    a

---
+++ a.py
@@ -0,0 +1,14 @@
+import json
+from collections import Counter
+from util import *
+
+# Your JSON array
+data = read_json('./dataset/parsed_merged.json')
+
+# Count languages
+language_counts = Counter(item["metadata"]["language"] for item in data)
+
+# Display the distribution
+print("Language Distribution:")
+for language, count in language_counts.items():
+    print(f"{language}: {count}")
